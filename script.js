46d6815783c66efd49f3065e492062d7c6c6f3e8
Muhammad Bilal Lodhi <bilalodhi01@gmail.com>
2026-01-31 10:29:01 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder for future JavaScript functionality
+// This file will contain:
+// - Smooth scrolling
+// - Mobile navigation toggle
+// - Scroll-triggered animations
+// - Form validation
+
+console.log('Portfolio loaded successfully!');
